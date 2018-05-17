db5fea44f8f434759946063c61ac75093a0ddf05
Artem <kackhotali@gmail.com>
2018-05-17 08:14:20 +0200

    refactoring class Garage, add IO in the file

--- src/fr/kacetal/Garage.java
+++ src/fr/kacetal/Garage.java
@@ -1,61 +1,88 @@
 package fr.kacetal;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import fr.kacetal.vehicule.Vehicule;
-
+import java.io.*;
+import java.nio.file.*;
+import java.util.*;
+import fr.kacetal.vehicule.*;
 
 public class Garage {
-	
+
 	private Set<Vehicule> voitures = new LinkedHashSet<>();
+
+	private String dirForFile = "./files";
+
+	private String fileForSave = "/garage.ser";
+
+	private Path mkDir = Paths.get(dirForFile);
+
+	private Path mkFile = Paths.get(dirForFile, fileForSave);
+
+	private void createFile() {
+		try {
+			if (!Files.isDirectory(mkDir)) {
+				Files.createDirectory(mkDir);
+				System.out.println("mkdir");
+			}
+			if (!Files.exists(mkFile)) {
+				Files.createFile(mkFile);
+				System.out.println("mkfile");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void saveObjToFile() {
+		createFile();
+		try (ObjectOutputStream objToSave = new ObjectOutputStream(
+				new BufferedOutputStream(new FileOutputStream(mkFile.toFile())))) {
+			for (Vehicule vhl : voitures) {
+				objToSave.writeObject(vhl);
+			}
+		} catch (IOException e) {
+			System.err.println("Problem with save objects in the file");
+			e.printStackTrace();
+		}
+	}
+
+	private void loadObjFromFile() {
+	    try (ObjectInputStream objToLoad = new ObjectInputStream(new BufferedInputStream(new FileInputStream(mkFile.toFile())))) {
+	        while (true) {
+	            voitures.add((Vehicule) objToLoad.readObject());
+	        }
+	    } catch (EOFException e2) {
 	
+	    } catch (ClassNotFoundException | IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+
+	public void addVoiture(Vehicule vehicule) {
+		voitures.add(vehicule);
+		createFile();
+		saveObjToFile();
+	}
+
 	@Override
 	public String toString() {
 		
+		createFile();
+		loadObjFromFile();
+
 		StringBuilder sb = new StringBuilder();
-		
-		sb.append("****************************\n");
-		sb.append("*  Garage OpenClassRooms   *\n");
-		sb.append("****************************\n");
-		
+
+		sb.append("***************************\n");
+		sb.append("*  Garage OpenClassRooms  *\n");
+		sb.append("***************************\n");
+
 		if (voitures.isEmpty()) {
 			System.err.println("Aucune voiture sauvegardée !\n");
 			return sb.toString();
-		}		
-		
+		}
+
 		for (Vehicule vehicule : voitures) {
 			sb.append(" + ").append(vehicule).append("\n");
 		}
 		return sb.toString();
 	}
-
-	public void addVoiture(Vehicule vehicule) {
-		createFile();
-		voitures.add(vehicule);
-	}
-	
-	private void createFile() {
-		try {
-			
-			Path mkDir = Paths.get("./files");
-			
-			Path mkFile = Paths.get("./files/Garage.txt");
-			
-			if (!Files.isDirectory(mkDir)) {
-				Files.createDirectory(mkDir);
-			}
-			
-			if (!Files.exists(mkFile)) {
-				Files.createFile(mkFile);
-			}			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}		 
-	}	
 }
